c9e0cbc5e8b9600877b4ebf850898fb2fe2447e0
Karol Moroz <k.j.moroz@gmail.com>
2022-10-29 01:49:21 +0200

    Add categories to product page

--- pages/menu/product/[slug].tsx
+++ pages/menu/product/[slug].tsx
@@ -1,13 +1,27 @@
 import { listCategoriesQuery } from "@api/queries";
+import { Category } from "@interfaces";
 import Layout from "@layout";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 
-interface Props {}
+interface Props {
+  categories: Category[];
+}
 
-const ProductPage: React.FC<Props> = () => {
+const ProductPage: React.FC<Props> = ({ categories }) => {
+  const { query } = useRouter();
+
+  const backUrl = `/menu/${query.ref ?? ""}`;
+
+  const activeCategory = query.ref
+    ? categories.find((c) => c.slug === query.ref)
+    : categories[0];
+
   return (
-    <Layout>
+    <Layout categories={categories}>
+      <Link href={backUrl}>&lt;&lt; {activeCategory?.namePl}</Link>
       <p>Hello world!</p>
     </Layout>
   );
