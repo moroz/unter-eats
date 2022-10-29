133307a8a5fb0fdd782e073dfc074c8310b2b6aa
Karol Moroz <k.j.moroz@gmail.com>
2022-10-29 03:09:53 +0200

    Add useHydrated in useCart

--- hooks/useCart.ts
+++ hooks/useCart.ts
@@ -1,6 +1,11 @@
 import { useContext } from "react";
 import CartContext from "@/lib/cart/CartContext";
+import useHydrated from "./useHydrated";
 
-const useCart = () => useContext(CartContext)!;
+const useCart = () => {
+  const hydrated = useHydrated();
+  const cart = useContext(CartContext)!;
+  return hydrated ? cart : { ...cart, items: [] };
+};
 
 export default useCart;
