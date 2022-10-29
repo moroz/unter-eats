dc89e9ae5826f3ef7f581b70d50b79dc27e1945a
Karol Moroz <k.j.moroz@gmail.com>
2022-10-30 01:30:40 +0200

    Cache last query result in useState

--- components/CartModal/index.tsx
+++ components/CartModal/index.tsx
@@ -1,42 +1,59 @@
 import useCart from "@/hooks/useCart";
 import { transformProducts, calculateTotal } from "@/lib/cart/CartHelpers";
 import { useGetProductsQuery } from "@api/queries";
-import React, { useMemo } from "react";
+import { Product } from "@interfaces";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import styles from "./CartModal.module.sass";
 
 interface Props {}
 
 const CartModal: React.FC<Props> = () => {
+  const [lastResult, setLastResult] = useState<Record<string, Product>>({});
+
   const { toggleCart, items, removeItem } = useCart();
   const ids = items.map((i) => i.id);
   const { data } = useGetProductsQuery(ids);
-  const products = transformProducts(data?.products);
+
+  useEffect(() => {
+    if (!data?.products) return;
+    setLastResult(transformProducts(data.products));
+  }, [data]);
+
   const grandTotal = useMemo(() => {
     if (!data?.products) return 0;
     return calculateTotal(items, data?.products);
   }, [items, data?.products]);
 
-  if (!products) return null;
+  if (!lastResult) return null;
 
   return (
-    <div className={styles.modal}>
-      {data && (
-        <ul>
-          {items.map(({ id, quantity }) => {
-            const product = products[id];
-            if (!product) return null;
-            return (
-              <li key={id}>
-                {product.namePl}, {product.price} &times; {quantity}
-                <button onClick={() => removeItem(id)}>Remove</button>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-      <p>Grand total: {grandTotal}</p>
-      <button onClick={toggleCart}>Close cart</button>
-    </div>
+    <>
+      <div className={styles.overlay} onClick={toggleCart} />
+      <div className={styles.modal}>
+        <button onClick={toggleCart} className={styles.close}>
+          &times;
+        </button>
+        <section className={styles.content}>
+          {lastResult && (
+            <ul>
+              {items.map(({ id, quantity }) => {
+                const product = lastResult[id];
+                if (!product) return null;
+                return (
+                  <li key={id}>
+                    {product.namePl}, {product.price} &times; {quantity}
+                    <button onClick={() => removeItem(id)}>Remove</button>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </section>
+        <section className={styles.summary}>
+          <p>Grand total: {grandTotal}</p>
+        </section>
+      </div>
+    </>
   );
 };
 
