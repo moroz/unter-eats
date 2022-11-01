708821451e272a989b6b2aecad3228f65785cffd
Karol Moroz <k.j.moroz@gmail.com>
2022-11-02 00:58:05 +0100

    Style Stripe elements to look exactly like custom buttons

--- components/StripeProvider/index.tsx
+++ components/StripeProvider/index.tsx
@@ -12,6 +12,8 @@ const stripePromise = loadStripe(
 );
 
 const buildTheme = (): Appearance => {
+  if (typeof window === "undefined") return {};
+
   const computed = getComputedStyle(document.body);
 
   return {
@@ -23,6 +25,15 @@ const buildTheme = (): Appearance => {
       colorText: computed.getPropertyValue("--stripe-label-color"),
       colorTextSecondary: computed.getPropertyValue("--fg-color"),
       colorTextPlaceholder: computed.getPropertyValue("--input-placeholder")
+    },
+    rules: {
+      ".Input": {
+        borderColor: computed.getPropertyValue("--fg-color"),
+        boxShadow: "none"
+      },
+      ".Input:focus": {
+        borderColor: computed.getPropertyValue("--primary")
+      }
     }
   };
 };
@@ -33,14 +44,15 @@ interface Props {
 }
 
 const StripeProvider: React.FC<Props> = ({ amount, children }) => {
-  const [mutate, { data }] = useCreatePaymentIntentMutation();
+  // const [mutate, { data }] = useCreatePaymentIntentMutation();
 
-  useEffect(() => {
-    mutate({ variables: { amount } });
-  }, [mutate]);
+  // useEffect(() => {
+  //   mutate({ variables: { amount } });
+  // }, [mutate]);
 
-  if (!data) return null;
-  const clientSecret = data.result.clientSecret;
+  // if (!data) return null;
+  const clientSecret =
+    "pi_3LzQzDAoLiH0r9L91GiV66Lx_secret_Vez8Nrp01YBc6yeX0WGGpMl27";
 
   return (
     <Elements
--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Layout from "@layout";
-import { Button, CheckoutLayout } from "@components";
+import {
+  Button,
+  CheckoutLayout,
+  PaymentForm,
+  StripeProvider
+} from "@components";
 import useHydrated from "@hooks/useHydrated";
 import useCart from "@hooks/useCart";
 import { useCartProductsQuery } from "@api/queries";
@@ -20,33 +25,38 @@ const Checkout: React.FC<Props> = () => {
 
   return (
     <CheckoutLayout title="Checkout">
-      <FormWrapper {...methods} className="container">
-        <InputField
-          {...register("fullName")}
-          label="Imię i nazwisko"
-          required
-        />
-        <InputGroup columns={2}>
+      <InputGroup columns={2}>
+        <FormWrapper {...methods}>
           <InputField
-            {...register("email")}
-            label="E-mail"
-            helperText="Na ten adres otrzymasz potwierdzenie zamówienia. Nie wysyłamy treści reklamowych."
+            {...register("fullName")}
+            label="Imię i nazwisko"
             required
           />
+          <InputGroup columns={2}>
+            <InputField
+              {...register("email")}
+              label="E-mail"
+              helperText="Na ten adres otrzymasz potwierdzenie zamówienia. Nie wysyłamy treści reklamowych."
+              required
+            />
+            <InputField
+              {...register("phoneNo")}
+              label="Telefon kontaktowy"
+              helperText="Pod ten numer będziemy dzwonić w razie pytań dotyczących dostawy."
+              required
+            />
+          </InputGroup>
           <InputField
-            {...register("phoneNo")}
-            label="Telefon kontaktowy"
-            helperText="Pod ten numer będziemy dzwonić w razie pytań dotyczących dostawy."
+            {...register("shippingAddress")}
+            label="Adres dostawy"
             required
           />
-        </InputGroup>
-        <InputField
-          {...register("shippingAddress")}
-          label="Adres dostawy"
-          required
-        />
-        <Button type="submit">Zamawiam z obowiązkiem zapłaty</Button>
-      </FormWrapper>
+          <Button type="submit">Zamawiam z obowiązkiem zapłaty</Button>
+        </FormWrapper>
+        <StripeProvider amount={2136}>
+          <PaymentForm amount={2136} />
+        </StripeProvider>
+      </InputGroup>
     </CheckoutLayout>
   );
 };
