dff45200b0cc52816d561390f873225469f994e7
Karol Moroz <k.j.moroz@gmail.com>
2022-11-13 16:20:47 +0100

    Set sizes on images in product grid

--- components/ProductImage/index.tsx
+++ components/ProductImage/index.tsx
@@ -2,13 +2,15 @@ import { Product } from "@interfaces";
 import React from "react";
 import styles from "./ProductImage.module.sass";
 import Image from "next/image";
-import biryani from "./biryani.jpg";
 
 interface Props {
   product?: Product;
   aspectRatio?: string;
   showPrice?: boolean;
 }
+
+const uuid = "c84ac4ff-bec1-4389-9dbb-5b971a154e54";
+const asset_host = "https://d19gk9i6dczo7b.cloudfront.net";
 
 const ProductImage: React.FC<Props> = ({
   product,
@@ -18,10 +20,21 @@ const ProductImage: React.FC<Props> = ({
   const style = aspectRatio
     ? ({ "--aspect-ratio": aspectRatio } as React.CSSProperties)
     : undefined;
+  const base_dir = `${asset_host}/images/${uuid.slice(0, 2)}/${uuid}`;
 
   return (
     <picture className={styles.root} style={style}>
-      <Image src={biryani} alt={product?.descriptionPl ?? ""} />
+      <source
+        type="image/webp"
+        media="(min-width: 59.25rem)"
+        srcSet={`${base_dir}/thumb.webp 1x, ${base_dir}/thumb_retina.webp 2x`}
+      />
+      <source
+        type="image/webp"
+        media="(max-width: 59.1875rem)"
+        srcSet={`${base_dir}/thumb_mobile.webp 1x, ${base_dir}/thumb_mobile_retina.webp 2x`}
+      />
+      <img src={`${base_dir}/thumb_mobile.webp`} alt="" />
       {product?.price && showPrice ? (
         <span className={styles.priceLabel}>{product.price} zł</span>
       ) : null}
